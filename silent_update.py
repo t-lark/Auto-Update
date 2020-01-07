f45f840795306a8cbbd77cc65afa5ace4c30147c
Tom Larkin <tom.larkin80@gmail.com>
2020-01-07 11:08:43 -0800

    changed comment code typo

--- silent_update.py
+++ silent_update.py
@@ -2,7 +2,7 @@
 """
 This script will silently patch any app by bundle ID, but only if the app itself is not running
 you must supply the bundle ID of the app to check and the policy event manual trigger for jamf as
-positional parameters 3 and 4
+positional parameters 4 and 5
 
 author:  tlark
 
